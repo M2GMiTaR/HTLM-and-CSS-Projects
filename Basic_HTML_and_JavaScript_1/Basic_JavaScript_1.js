95aa684c9bb69bb0c21e9a0f86ebb0de221ff7e5
Grega <mirticgrega.dd@gmail.com>
2025-10-03 15:35:57 +0100

    Created Basic html 2 and Basic JS
    Created Basic HTML 2 that calls a script function from a separate Java Script file (Basic_JavaScript_1)

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+
+		function My_First_Function() {
+			var String = "Kiss me, I'm Slovenian!";
+			var result = String.fontcolor("blue");
+			document.getElementById("Slovenian").innerHTML =
+			result;
+		}
